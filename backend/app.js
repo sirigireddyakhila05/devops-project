d78938d8861f953212e03c69682120308234102b
Akhila Sirigireddy <akhilasirigireddy05@gmail.com>
2026-05-22 16:51:34 +0530

    Fix Render deployment without Docker

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,12 @@
-console.log("🚀 DevOps Project Running Successfully in VS Code");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("🚀 DevOps Project is Live on Cloud!");
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
