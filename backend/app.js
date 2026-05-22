29a21996d9fe9dcf7b4cc6b08829c2ed0c3c6a1c
Akhila Sirigireddy <akhilasirigireddy05@gmail.com>
2026-05-22 18:20:09 +0530

    fix app.js structure issue

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("CI/CD FINAL TEST SUCCESS 🚀");
-    app.listen(PORT, "0.0.0.0", () => {
-        console.log("Server running on port " + PORT);
-    });
+    res.send("🚀 CI/CD FINAL TEST SUCCESS 🚀");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on port " + PORT);
+});
