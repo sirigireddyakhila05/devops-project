5aeabf44ca03638d04147d04c164163d9709c79e
Akhila Sirigireddy <akhilasirigireddy05@gmail.com>
2026-05-22 18:14:17 +0530

    final ci/cd test

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("🚀 Updated CI/CD working!");
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log("Server running on port " + PORT);
-});
+    res.send("CI/CD FINAL TEST SUCCESS 🚀");
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log("Server running on port " + PORT);
+    });
